562df05c99be28a6838c277bbab07eed8d4aa090
Shan Gray <shan.gray@ogilvy.co.za>
2021-06-04 13:13:39 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -1,24 +1,15 @@
-function smoothy() {
-  $('a[href*="#"]:not([href="#"])').on('click',function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      let target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        setTimeout(function(){
-          const yOffset = -213;
-          const element = document.getElementById(target);
 
-          window.scrollTo({top: target, behavior: 'smooth'});
-
-        }, 900);
-        return false;
-      }
-    }
-  });
-}
 document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("touchstart", function(e) {passive: true} );
 
-  smoothy();
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
 
 });
